cd1ec0884e89c90cfb22106d9e5301be0bd1d0ed
ahmedfaroUk1oo <ahmedelmahdy33072@gmail.com>
2021-12-01 17:06:43 +0200

    https://github.com/awny277/elmahalla-doctors

--- src/components/Doctors/DashBoard/Booking/PatientAppoinment/UpComing.jsx
+++ src/components/Doctors/DashBoard/Booking/PatientAppoinment/UpComing.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
-import "./index.css"
+import { NavLink } from 'react-router-dom';
+import "./index.css";
 const UpComing = () => {
+  let patient = [
+    { name: "Richard Wilson ", id: "#PT0016", date: "11 Nov 2019",time:"10.00 AM",type:"old Patient",paid:"$150", imgUrl: "patient.jpg" },
+    { name: "Richard Wilson ", id: "#PT0016", date: "11 Nov 2019",time:"10.00 AM",type:"New Patient",paid:"$200", imgUrl: "patient1.jpg" },
+    { name: "Richard Wilson ", id: "#PT0016", date: "11 Nov 2019",time:"10.00 AM",type:"New Patient",paid:"$75", imgUrl: "patient2.jpg" },
+  ] 
   return (
-    <h1>hello from UpComing</h1>
+    <NavLink to="/"></NavLink>
   );
 }
 
