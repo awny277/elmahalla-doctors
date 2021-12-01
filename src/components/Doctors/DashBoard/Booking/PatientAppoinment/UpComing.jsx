93a19f2b2e5ad606810b9a3fd0c16704e66b3442
awny277 <at.awny@yahoo.com>
2021-12-01 17:11:54 +0200

    https://github.com/awny277/elmahalla-doctors

--- src/components/Doctors/DashBoard/Booking/PatientAppoinment/UpComing.jsx
+++ src/components/Doctors/DashBoard/Booking/PatientAppoinment/UpComing.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { patient } from '../../../patient/patientDetails';
 import "./index.css";
+
 const UpComing = () => {
-  let patient = [
-    { name: "Richard Wilson ", id: "#PT0016", date: "11 Nov 2019",time:"10.00 AM",type:"old Patient",paid:"$150", imgUrl: "patient.jpg" },
-    { name: "Richard Wilson ", id: "#PT0016", date: "11 Nov 2019",time:"10.00 AM",type:"New Patient",paid:"$200", imgUrl: "patient1.jpg" },
-    { name: "Richard Wilson ", id: "#PT0016", date: "11 Nov 2019",time:"10.00 AM",type:"New Patient",paid:"$75", imgUrl: "patient2.jpg" },
-  ] 
+
   return (
-    <NavLink to="/"></NavLink>
+    <React.Fragment>
+      <div>
+        {patient.map((ele, indx) => (
+          <p>{ele.name}</p>
+        ))}
+      </div>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/Doctors/patient/patientDetails.jsx
@@ -0,0 +1,5 @@
+export let patient = [
+  { name: "Richard Wilson ", id: "#PT0016", date: "11 Nov 2019", time: "10.00 AM", type: "old Patient", paid: "$150", imgUrl: "patient.jpg" },
+  { name: "Richard Wilson ", id: "#PT0016", date: "11 Nov 2019", time: "10.00 AM", type: "New Patient", paid: "$200", imgUrl: "patient1.jpg" },
+  { name: "Richard Wilson ", id: "#PT0016", date: "11 Nov 2019", time: "10.00 AM", type: "New Patient", paid: "$75", imgUrl: "patient2.jpg" },
+]
