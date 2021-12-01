d57ac824999792ce6366db17ce842b7d5a4dc9d6
awny277 <at.awny@yahoo.com>
2021-12-01 15:18:29 +0200

    https://github.com/awny277/elmahalla-doctors

--- src/components/Doctors/DashBoard/DashSideBar/DashSideBar.jsx
+++ src/components/Doctors/DashBoard/DashSideBar/DashSideBar.jsx
@@ -10,8 +10,40 @@ const DashSideBar = () => {
         </div>
         <h3 className="text-center">Dr. Darren Elder</h3>
         <p className="text-center">BDS, MDS - Oral & Maxillofacial Surgery</p>
-        <NavLink to="/">لوحة التحكم</NavLink>
-        <NavLink to="/">المرضي</NavLink>
+        <ul>
+          <li>
+            <i className="fas fa-chart-line"></i>
+            <NavLink to="/">لوحة التحكم</NavLink>
+          </li>
+          <li>
+            <i className="fas fa-calendar-check"></i>
+            <NavLink to="/">الحجز اليومي</NavLink>
+          </li>
+          <li>
+            <i className="fas fa-hospital-user"></i>
+            <NavLink to="/">المرضي</NavLink>
+          </li>
+          <li>
+            <i className="fas fa-clock"></i>
+            <NavLink to="/">جدول المواعيد</NavLink>
+          </li>
+          <li>
+            <i className="fas fa-file-invoice-dollar"></i>
+            <NavLink to="/">الفواتير</NavLink>
+          </li>
+          <li>
+            <i className="fas fa-user-cog"></i>
+            <NavLink to="/">اعدادات الملف الشخصي</NavLink>
+          </li>
+          <li>
+            <i className="fas fa-share-alt"></i>
+            <NavLink to="/">وسائل التواصل الاجتماعي</NavLink>
+          </li>
+          <li>
+            <i className="fas fa-sign-in-alt"></i>
+            <NavLink to="/">تسجيل الخروج</NavLink>
+          </li>
+        </ul>
       </div>
     </React.Fragment>
   );
