ec00898f502233529a4847cd9f9792657bce1262
ahmedfaroUk1oo <ahmedelmahdy33072@gmail.com>
2021-12-01 15:19:32 +0200

    https://github.com/awny277/elmahalla-doctors

--- src/components/Doctors/DashBoard/Booking/Booking.jsx
+++ src/components/Doctors/DashBoard/Booking/Booking.jsx
@@ -1,6 +1,29 @@
 import React from 'react';
+import './index.css';
 const BookingDoctor = () => {
-  return (<h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis earum delectus inventore voluptatum, autem ipsam cumque reprehenderit adipisci, soluta iure vel iusto nulla. At fuga soluta, in aliquam doloribus quisquam!</h1>);
+  let total = [
+    {name:"Total Patient",number:1500,date:"Till Today",imgUrl:"icon-01.png"},
+    {name:"Today Patient",number:160,date:"06, Nov 2019",imgUrl:"icon-02.png" },
+    {name:"Appoinments",number:85,date:"06, Apr 2019",imgUrl:"icon-03.png"}
+  ]
+  return (
+    <React.Fragment>
+      <div className="total">
+        {total.map((ele ,indx)=>(
+          <div className="total-sections" key={indx}  id={`total-sections${indx}`}>
+            <div className="img-section"  id={`total${indx}`}>
+            <img src={`/img/${ele.imgUrl}`} alt="" />
+            </div>
+            <div className="details">
+            <h6>{ele.name}</h6>
+            <h3>{ele.number}</h3>
+            <p>{ele.date}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default BookingDoctor;
